adb6ba78bb1fee2cdf1430215f1f597dfcb0b5ff
hardarmyyy <adeyemoadams005@gmail.com>
2026-03-13 23:47:11 +0000

    UPDATE: add dashboard app

---
+++ frontend/dashboard/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+
+# Create your views here.
+@login_required
+@never_cache
+def dashboard_index(request):
+    return render(request, 'dashboard/dashboard_index.html', {'show_logout_button': True})
